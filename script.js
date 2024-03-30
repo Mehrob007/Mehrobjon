c689e6e14b7ed26767b23199a817007c84fbc79a
Mehrob <your_email@example.com>
2024-03-30 16:15:15 +0500

    commit 3

--- script.js
+++ script.js
@@ -16,37 +16,37 @@ const root = document.getElementById('root');
 
 
 
-const buttonSend = document.createElement('button');
-buttonSend.id = 'sendButton';
-buttonSend.textContent = 'send';
-root.appendChild(buttonSend)
+// const buttonSend = document.createElement('button');
+// buttonSend.id = 'sendButton';
+// buttonSend.textContent = 'send';
+// root.appendChild(buttonSend)
 
-document.getElementById("sendButton").addEventListener("click", function() {
-    const dataToSend = {
-      key1: 'value1',
-      key2: 'value2'
-    };
+// document.getElementById("sendButton").addEventListener("click", function() {
+//     const dataToSend = {
+//       key1: 'value1',
+//       key2: 'value2'
+//     };
     
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(dataToSend)
-    };
+//     const requestOptions = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(dataToSend)
+//     };
     
-    fetch('https://126b63f3395fce52.mokky.dev/user', requestOptions)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Network response was not ok, status: ${response.status}, ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('There was a problem with your fetch operation:', error);
-      });
-  });
+//     fetch('https://126b63f3395fce52.mokky.dev/user', requestOptions)
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error(`Network response was not ok, status: ${response.status}, ${response.statusText}`);
+//         }
+//         return response.json();
+//       })
+//       .then(data => {
+//         console.log(data);
+//       })
+//       .catch(error => {
+//         console.error('There was a problem with your fetch operation:', error);
+//       });
+//   });
   
